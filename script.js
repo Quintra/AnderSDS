64879fd1014f87bfe751de32f00ecfa6fc454435
Quintra <andesonds209@gmail.com>
2022-03-18 18:17:44 -0300

    80%

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this .scrollY > 20){
+        if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        if(this .scrollY > 500){
+        if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
@@ -15,7 +15,40 @@ $(document).ready(function(){
         $('html').animate({scrollTop:0});
      });
 
-     /*-------mudar cor-------*/
+    var typed = new Typed(".typing", {
+    strings:["Meu nome é"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+    });
+
+    //toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    })
+});
+     /*-------mudar cor-------
 
     $(window).scroll(function() { 
     var scroll = $(window).scrollTop();
@@ -33,38 +66,4 @@ $(document).ready(function(){
     }
     });
 
-         /*----mudar cor-------*/
-
-    var typed = new Typed(".typing", {
-        strings:["Meu nome é"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
-    });
-
-    //toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-    $('.carousel').owlCarousel({
-        margin:20,
-        loop:20,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:false
-            }
-        }
-    });
-});
+         ----mudar cor-------*/
