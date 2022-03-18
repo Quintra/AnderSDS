63f64e69bb0111debf01b4a481c827e6540903e2
Quintra <andesonds209@gmail.com>
2022-03-18 19:59:09 -0300

    80%

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
+            if(this.scrollY > 20){
+                $('.navbar').addClass("sticky");
+            }else{
+                $('.navbar').removeClass("sticky"); 
         }
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
@@ -11,7 +11,7 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
-     $('.scroll-up-btn').click(function(){
+    $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop:0});
      });
 
@@ -22,8 +22,7 @@ $(document).ready(function(){
     loop:true
     });
 
-    //toggle menu/navbar script
-    $('.menu-btn').click(function(){
+$('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
